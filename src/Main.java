5dead4eec22b0f5b545806cf487abc8d72eb825a
hongmj37 <alswjd3813@naver.com>
2022-05-22 21:11:52 +0900

    String 실습

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome!");
+        print("Welcome, jeju");
+    }
+
+    static void print(String input){
+        System.out.println(input);
+    }
+}
