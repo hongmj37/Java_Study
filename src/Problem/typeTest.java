3bd83623fa899bb6fe6a94030259453727167aa6
hongmj37 <alswjd3813@naver.com>
2022-05-23 16:13:45 +0900

    no message

--- src/Problem/typeTest.java
+++ src/Problem/typeTest.java
@@ -5,7 +5,7 @@ public class typeTest {
         int num = 1 + '1';  //문자 '1'은 숫자로 변환되어 49로 바뀜
         System.out.println(num);
 
-        char num2 = 1 + '1';  
+        char num2 = 1 + '1';
         System.out.println(num2);
     }
 }
