00bd16a28d6e4fb253af5c0e649db20fd4c9264e
hongmj37 <alswjd3813@naver.com>
2022-05-23 14:56:29 +0900

    아스키코드 이해

---
+++ src/Problem/typeTest.java
@@ -0,0 +1,11 @@
+package Problem;
+
+public class typeTest {
+    public static void main(String[] args) {
+        int num = 1 + '1';  //문자 '1'은 숫자로 변환되어 49로 바뀜
+        System.out.println(num);
+
+        char num2 = 1 + '1';  
+        System.out.println(num2);
+    }
+}
