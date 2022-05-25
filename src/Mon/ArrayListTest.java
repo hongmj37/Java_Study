956d23d77ff69301742149806ead4fb987005c0f
hongmj37 <alswjd3813@naver.com>
2022-05-25 17:04:16 +0900

    no message

--- src/Mon/ArrayListTest.java
+++ src/Mon/ArrayListTest.java
@@ -10,7 +10,7 @@ public class ArrayListTest {
 
         ls.add("crown");
         ls.add("lotte");
-        ls.add("orion");
+        ls.add("orion"); //추가
 
         for(int i=0; i < ls.size(); i++){
             String need = ls.get(i);
