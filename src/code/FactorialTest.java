e7a1760fb0980974547e0e9b85dcd3e39656b48e
hongmj37 <alswjd3813@naver.com>
2022-05-26 10:20:10 +0900

    팩토리얼 실습

---
+++ src/code/FactorialTest.java
@@ -0,0 +1,17 @@
+package code;
+
+public class FactorialTest {
+    public static void main(String[] args) {
+        int input = 5;
+
+        System.out.println(fact(input));
+    }
+
+    public static int fact(int n){
+        if (n<=1){
+            return  n;
+        } else{
+            return fact(n-1)*n;
+        }
+    }
+}
