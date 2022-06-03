fae32c42f5b0e583245ddfabfc75e5f9b48cb402
hongmj37 <alswjd3813@naver.com>
2022-06-03 18:26:32 +0900

    수정

--- src/code/pairTest2.java
+++ src/code/pairTest2.java
@@ -6,22 +6,21 @@ import java.util.Queue;
 
 public class pairTest2 {
     public static void main(String[] args) {
-        int bufferSize = 2;
-        int capacities = 10;
-        int[] documents = new int[]{7, 4, 5, 6};
+        int bufferSize = 3;
+        int capacities = 15;
+        int[] documents = new int[]{7, 4, 5, 6, 8};
 
         int output = queuePrinter(bufferSize, capacities, documents);
-        System.out.println(output); // 8
+        System.out.println(output);
     }
     public static int queuePrinter(int bufferSize, int capacities, int[] documents) {
-        // TODO:
         Queue<Integer> queue = new LinkedList<>();
         int time = 0;
-        for(int i = 0; i < bufferSize; i++) { // 큐를 버퍼사이즈만큼 0으로 채워줍니다.
+        for(int i = 0; i < bufferSize; i++) {
             queue.add(0);
         }
         queue.poll();
-        queue.add(documents[0]);    //첫번째 데이터 삽입
+        queue.add(documents[0]);
         time++;
         documents = Arrays.copyOfRange(documents, 1, documents.length);
 
