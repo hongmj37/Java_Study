51790cb046c8e506df171bb9c9f36e669f9cc578
hongmj37 <alswjd3813@naver.com>
2022-06-06 11:09:58 +0900

    백준 9498 시험성적 문제 풀기

---
+++ src/backjoon/hong_9498.java
@@ -0,0 +1,24 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class hong_9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        String result;
+
+        if (score >= 90 && score <= 100) {
+            result = "A";
+        } else if (score >= 80 && score <= 89){
+            result = "B";
+        }  else if (score >= 70 && score <= 79) {
+            result = "C";
+        } else if (score >= 60 && score <= 69) {
+            result = "D";
+        } else {
+            result = "F";
+        }
+        System.out.println(result);
+    }
+}
