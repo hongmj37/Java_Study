4816659c7f1ef61766bdaf511a34fcd34593ed73
hongmj37 <alswjd3813@naver.com>
2022-06-12 08:41:54 +0900

    백준 10950번 문제 풀기

---
+++ src/backjoon/hong_10950.java
@@ -0,0 +1,17 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class hong_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        for(int i = 0; i < x; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+
+    }
+}
