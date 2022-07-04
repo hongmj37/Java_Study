a9a6b8317fcdae58c75d0be7fb5eb911d1909efb
hongmj37 <alswjd3813@naver.com>
2022-07-04 22:17:19 +0900

    주석 달기

--- src/backjoon/문자열/hong_1157.java
+++ src/backjoon/문자열/hong_1157.java
@@ -17,25 +17,25 @@ import java.util.stream.IntStream;
 
 public class hong_1157 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String str = bf.readLine().toUpperCase();
-        StringTokenizer st = new StringTokenizer(str, "");
-        String[] arr = new String[st.countTokens()];
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));  //입력받고
+        String str = bf.readLine().toUpperCase();                       //대문자로 변경
+        StringTokenizer st = new StringTokenizer(str, "");      //문자를 하나 하나 분리시킴
+        String[] arr = new String[st.countTokens()];                // 분리된 문자열을 배열에 하나씩 담음
         for(int i=0; i< st.countTokens(); i++) {
             arr[i] = st.nextToken();
         }
 
-        Map<String, Integer> temp = new HashMap<>();
+        Map<String, Integer> temp = new HashMap<>();     //"키" : "값" 의 형태의 맵을 사용하여 각 알파벳의 수를 저장하고자 함
 
         for(int i=1; i<arr.length; i++){
-            String base = arr[i];
+            String base = arr[i];               //만약 zZa이면 (ZZA) Z부터 비교 시작
             int count = 0;
             for(int j=0; j<arr.length; j++){
-               if(base.equals(arr[j])){
+               if(base.equals(arr[j])){        //z와 같은 값이 있다면 count를 + tlzla
                    count++;
                }
              }
-            temp.put(arr[i],count);
+            temp.put(arr[i],count);             //temp라는 map에 알파벳과 개수 저장 (내예상 시나리오는 Z:2, Z:2, A:1)
         }
         String result = "";
 
@@ -51,5 +51,6 @@ public class hong_1157 {
     }
 }
 
+//그래서 가장 큰 value를 가지는 값의 key를 뽑아 결과를 출력하고 싶었음
 
 // 시류ㅐㅁㄹ;아ㅓㄴ실패
