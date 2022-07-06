764ac849d5032f3a291d4f816efad935576e4140
hongmj37 <alswjd3813@naver.com>
2022-07-06 17:11:55 +0900

    백준 10926번 ??!

---
+++ src/backjoon/입출력과사칙연산/Q10926.java
@@ -0,0 +1,17 @@
+package backjoon.입출력과사칙연산;
+/* ??!
+time : 124 ms
+memory : 14304 KB
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+
+        System.out.println(str + "??!");
+    }
+}
