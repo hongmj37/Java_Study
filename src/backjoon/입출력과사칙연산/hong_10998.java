f443f5e98d8f22850c9755f91e83c88088250d2a
hongmj37 <alswjd3813@naver.com>
2022-07-08 10:08:12 +0900

    백준 10998번 AXB

---
+++ src/backjoon/입출력과사칙연산/hong_10998.java
@@ -0,0 +1,23 @@
+package backjoon.입출력과사칙연산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* AXB
+time: 124 ms
+memory: 14216 KB
+
+ */
+public class hong_10998 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A*B);
+
+    }
+}
