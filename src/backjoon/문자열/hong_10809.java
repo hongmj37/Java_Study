46abd2980a300391de427b96f45a8fb2da614730
hongmj37 <alswjd3813@naver.com>
2022-07-18 22:07:25 +0900

    no message

--- src/backjoon/문자열/hong_10809.java
+++ src/backjoon/문자열/hong_10809.java
@@ -1,10 +1,23 @@
 package backjoon.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /* 알파벳 찾기
 time :
 memory :
  */
 public class hong_10809 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(),"");
+
+        for(int i=0; i< st.countTokens(); i++){
+
+            System.out.print(i);
+        }
 
     }
 }
