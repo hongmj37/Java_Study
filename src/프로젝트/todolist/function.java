768ace68c7eca4843bb0449ee4edcb8cd7840a8a
hongmj37 <alswjd3813@naver.com>
2022-07-21 17:00:41 +0900

    no message

---
+++ src/프로젝트/todolist/function.java
@@ -0,0 +1,12 @@
+package 프로젝트.todolist;
+
+public class function {
+    /* 필요 기능
+    * 1. todo 리스트 목록에 아이템 추가
+    * 2. todo 리스트 목록 중 특정 아이템 조회
+    * 3. todo 리스트 전체 목록 조회
+    * 4. todo 리스트 목록 중 특정 아이템 수정
+    * 5. todo 리스트 목록 중 특정 아이템 삭제
+    * 6. todo 리스트 전체 목록 삭제
+    * */
+}
