1cc9453012195e31800e26c80abdfb0317f908cc
hongmj37 <alswjd3813@naver.com>
2022-07-25 17:33:10 +0900

    프로그래머스 1 전화번호 * 처리

---
+++ src/프로그래머스/레벨1/pro2.java
@@ -0,0 +1,14 @@
+package 프로그래머스.레벨1;
+
+public class pro2 {
+    public static void main(String[] args) {
+        String phone_number = "027778888";
+        String answer = "";
+        for(int i=0; i<phone_number.length()-4; i++){
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length()-4,phone_number.length());
+        System.out.println(answer);
+    }
+
+}
