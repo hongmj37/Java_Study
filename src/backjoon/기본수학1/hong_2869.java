2cbcb41eac73b8cfbbc30e7d84711cc4d8f9a264
hongmj37 <alswjd3813@naver.com>
2022-07-29 15:09:01 +0900

    no message

--- src/backjoon/기본수학1/hong_2869.java
+++ src/backjoon/기본수학1/hong_2869.java
@@ -1,10 +1,21 @@
 package backjoon.기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /* 달팽이는 올라가고 싶다
 time :
 memory :
  */
 public class hong_2869 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+        int V = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
     }
 }
