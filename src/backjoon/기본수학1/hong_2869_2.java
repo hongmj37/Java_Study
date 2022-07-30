be19a585ffd091254a025fc03b38f997ae77383e
hongmj37 <alswjd3813@naver.com>
2022-07-31 08:56:32 +0900

    no message

--- src/backjoon/기본수학1/hong_2869_2.java
+++ src/backjoon/기본수학1/hong_2869_2.java
@@ -17,24 +17,21 @@ public class hong_2869_2 {
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
         int A = Integer.parseInt(st.nextToken());  //낮 +
         int B = Integer.parseInt(st.nextToken()); //밤 -
-        int V = Integer.parseInt(st.nextToken()); //높dl
-
-        int base = V - B;
-        int date = A - B;
-        int result = base / date;
+        int V = Integer.parseInt(st.nextToken()); //높이
+        //마지막 날에는 미끄러지는 부분이 없는 것으로 결과적인 목표는 V-B인 꼴 !!
+        int base = V - B;  //정싱에 도달했을 때 미끄러지지 않기 때문에 즉, 계산이 수행되지 않도록 미리 미끄럼을 빼줌
+        int date = A - B;  //하루에 올라간 거리 : up - down
+        int result = base / date; 
 
         if (base % date != 0){
-            result ++;
+            result += 1;
+            //정확히 나누어 떨어지지 않는다면 하루를 더 올라간 셈이니깐 +1 해주기
         }
         System.out.println(result);
     }
 }
 /*
-0 + 2 = 2, 2 - 1 = 1
-1 + 2 = 3, 3 - 1 = 2
-2 + 2 = 4, 4 - 1 = 3
-3 + 2 = 5, 5 - 1 = 4  (*)
-4 + 2 = 6, 6 - 1 = 5
-
-
+3 1 7 일 때, 결과는 3
+7 / (3 - 1) = 3.XXX
+(7 - 1) / (3 - 1) = 3
  */
