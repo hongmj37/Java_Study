51a57a34a0af4587267cca2725f20ea74bfbe524
hongmj37 <alswjd3813@naver.com>
2022-08-15 08:15:11 +0900

    백준 10809 알파벳 찾기

--- src/backjoon/문자열/hong_10809.java
+++ src/backjoon/문자열/hong_10809.java
@@ -3,21 +3,32 @@ package backjoon.문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 /* 알파벳 찾기
-time :
-memory :
+time : 144ms
+memory : 15820KB
  */
+
 public class hong_10809 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(),"");
+        String word = bf.readLine();
 
-        for(int i=0; i< st.countTokens(); i++){
+        int[] arr = new int[26];
 
-            System.out.print(i);
+        for(int i=0; i<arr.length; i++){
+            arr[i] = -1;
         }
 
+        for(int i=0; i<word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if(arr[ch-'a']==-1) {
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
     }
 }
