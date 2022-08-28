08dfb632b7aafb23fe859f469dc4cf7c0be039f8
hongmj37 <alswjd3813@naver.com>
2022-08-28 14:10:26 +0900

    split 테스트

---
+++ src/backjoon/반복문/hong_test.java
@@ -0,0 +1,9 @@
+package backjoon.반복문;
+
+public class hong_test {
+    public static void main(String[] args) {
+        String str = "Hello, It is tie to study";
+        String[] strArr = str.split(" ");
+        System.out.println(strArr);
+    }
+}
