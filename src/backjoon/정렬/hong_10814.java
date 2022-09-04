7d79fb34c76d994483e54237ea472aebfb4f0f50
hongmj37 <alswjd3813@naver.com>
2022-09-04 11:31:24 +0900

    백준 10814 나이순 정렬 풀 것

---
+++ src/backjoon/정렬/hong_10814.java
@@ -0,0 +1,14 @@
+package backjoon.정렬;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 나이순 정렬
+time : ms
+memory : KB
+ */
+public class hong_10814 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
