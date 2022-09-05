43ee5856e952f2f2fc6c0d0286b6c7d389bc706a
hongmj37 <alswjd3813@naver.com>
2022-09-05 20:13:24 +0900

    no message

--- src/backjoon/정렬/hong_10814.java
+++ src/backjoon/정렬/hong_10814.java
@@ -1,6 +1,7 @@
 package backjoon.정렬;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /* 나이순 정렬
@@ -8,7 +9,8 @@ time : ms
 memory : KB
  */
 public class hong_10814 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
     }
 }
