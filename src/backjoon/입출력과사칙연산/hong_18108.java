812925b59295462f68a8bf594dde9a4d17f3b8fd
hongmj37 <alswjd3813@naver.com>
2022-09-10 15:45:54 +0900

    no message

--- src/backjoon/입출력과사칙연산/hong_18108.java
+++ src/backjoon/입출력과사칙연산/hong_18108.java
@@ -1,12 +1,21 @@
 package backjoon.입출력과사칙연산;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 1998년생인 내가 태국에서는 2541년생?!
- * time :
- * memory : 
+ * time : 120ms
+ * memory : 14240KB
  */
 public class hong_18108 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int age = Integer.parseInt(bf.readLine());
+
+        int result = age - 543;
+        System.out.println(result);
 
     }
 }
